0c59d5d88b2709b1f5db31b50bb5046156359297
Gayatri <gayatri29mehta@gmail.com>
2023-09-01 09:22:11 +0530

    calc in java

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Calculator
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    float a=sc.nextFloat();
+	    float b=sc.nextFloat();
+	    add(a,b);
+	    sub(a,b);
+	    mul(a,b);
+	    div(a,b);
+	}
+
+	    
+	    public static void add(float a,float b){
+	        float c=a+b;
+	        System.out.println(c);
+	    }
+		
+		public static void sub(float a,float b){
+		    float d=a-b;
+		    System.out.println(d);
+		}
+		public static void mul(float a,float b){
+		    float e=a*b;
+		    System.out.println(e);
+		}
+		public static void div(float a,float b){
+		    float f=a/b;
+		    System.out.println(f);
+		}
+	
+
+}
